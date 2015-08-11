62395fd22219f99b4a0bfecd4a82497e3554ea5a
Kaede Fujisaki <kaede@hexe.net>
2015-08-12 00:17:45 +0900

    Signed-off-by: PSI <ledyba@users.osdn.me>

--- src/Main.hs
+++ src/Main.hs
@@ -11,8 +11,19 @@ import Java2js.Load
 import Java2js.Type
 import Data.String.Utils (endswith)
 
+--import Java2js.Java.JAR.Archive
+--import Java2js.JVM.Assembler
+
 buildClassPath = do
 		addJAR "sample/rhino-1.5r4.1.jar"
+{-
+		cls <- readFromJAR "sample/rhino-1.5r4.1.jar" "org/mozilla/javascript/tools/shell/Global.class"
+		print $ zip [1..] (fmap methodName (classMethods cls))
+		let (Just meth) = methodByName cls "runProcess"
+		let (Just bin) = (attrByName meth "Code")
+		let m = decodeMethod bin
+		print $ m --codeLength m
+-}
 
 main = do
 	entries <- execClassPath $ buildClassPath
