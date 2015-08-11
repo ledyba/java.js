53d4e12718007cb077e10c4bc04e7dcb7c89c403
Kaede Fujisaki <kaede@hexe.net>
2015-08-11 22:57:45 +0900

    loadJAR

--- lib/Java2js/Load.hs
+++ lib/Java2js/Load.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Java2js.Load(loadKlass) where
+module Java2js.Load(loadKlass, withKlass, loadCPEntry, loadCPEntries) where
 --
 
 import Data.ByteString.Lazy.Char8 (unpack)
 import Java2js.Type
+import Java2js.Java.ClassPath
+import Java2js.Java.JAR.Archive (readFromJAR)
 import Java2js.JVM.Common ()
 import Java2js.JVM.Converter
 import Java2js.JVM.ClassFile
@@ -11,6 +13,22 @@ import Java2js.JVM.Assembler
 import qualified Data.Set as S
 import qualified Data.Map.Strict as M
 
+loadCPEntries' :: String -> [Tree CPEntry] -> IO [Klass]
+loadCPEntries' prefix entries = fmap concat (mapM (loadCPEntry' prefix) entries)
+
+loadCPEntries :: [Tree CPEntry] -> IO [Klass]
+loadCPEntries = loadCPEntries' ""
+
+loadCPEntry' :: String -> Tree CPEntry -> IO [Klass]
+loadCPEntry' prefix (Directory dir left) = loadCPEntries' (prefix++dir++"/") left
+loadCPEntry' _ (File (Loaded _ cls)) = return [loadKlass cls]
+loadCPEntry' _ (File (LoadedJAR _ cls)) = return [loadKlass cls]
+loadCPEntry' prefix (File (NotLoaded f)) = fmap (\cls -> [loadKlass cls]) (parseClassFile (prefix++f))
+loadCPEntry' prefix (File (NotLoadedJAR jarfile path)) = fmap (\cls -> [loadKlass cls]) (readFromJAR jarfile (prefix++path))
+loadCPEntry :: Tree CPEntry-> IO [Klass]
+loadCPEntry = loadCPEntry' ""
+
+loadKlass :: Class Direct -> Klass
 loadKlass cls = Klass {
 	klassName = unpack (thisClass cls),
 	superKlass = unpack (superClass cls),
@@ -26,3 +44,13 @@ loadKlass cls = Klass {
 		extMeth meth = (unpack $ methodName meth, (methodSignature meth, extCode meth))
 		extCode :: Method Direct -> Maybe Code
 		extCode meth = fmap decodeMethod (attrByName meth "Code")
+
+withKlass :: [Tree CPEntry] -> String -> (Klass -> a) -> IO (Maybe a)
+withKlass entries klsName fun =
+			do
+				cls <- getEntry entries klsName
+				return $ fmap (fun.loadKlass.extract) cls
+			where
+				extract (Loaded _ cls) = cls
+				extract (LoadedJAR _ cls) = cls
+				extract _ = error "[BUG]"
--- src/Main.hs
+++ src/Main.hs
@@ -4,12 +4,19 @@ module Main where
 import Java2js.JVM.Common()
 import Java2js.JVM.ClassFile
 import Java2js.JVM.Converter
+import Java2js.Java.JAR
+import Java2js.Java.ClassPath
 import Java2js.Gen
 import Java2js.Load
 import Java2js.Type
+import Data.String.Utils (endswith)
+
+buildClassPath = do
+		addJAR "sample/rhino-1.5r4.1.jar"
 
 main = do
-	cls <- parseClassFile "sample/Pi.class"
-	let klass = loadKlass cls
-	print klass
-	putStrLn $ compileKlass klass
+	entries <- execClassPath $ buildClassPath
+	print entries
+	klasses <- loadCPEntries entries
+	let src = fmap compileKlass klasses
+	print src
